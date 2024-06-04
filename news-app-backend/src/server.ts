d78b9461b5ea5b17e00cc45d4669fbffa1a0da7e
Mark Blumenthal <markblumenthal@live.com>
2024-06-04 16:21:54 +0300

    changes

--- news-app-backend/src/server.ts
+++ news-app-backend/src/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import articlesRouter from './routes/articles';
 import userRouter from './routes/user';
 import commentsRouter from './routes/comments';
+import path from 'path'
 
 dotenv.config();
 
@@ -20,13 +21,13 @@ app.use('/api/articles', articlesRouter);
 app.use('/api/users', userRouter);
 app.use('/api/comments', commentsRouter);
 
-// // Have Node serve the files for our built React app
-// // app.use(express.static(path.resolve(__dirname, "./client/build")));
-// app.use(express.static(path.join(__dirname, "../news-app-backend/dist")));
-// // All other GET requests not handled before will return our React app
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../news-app-frontend/dist", "index.html"));
-// });
+// Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../news-app-backend/dist")));
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../news-app-frontend/dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
