48461c0d4b6e4652632b85198a515f6f7e77858b
Mark Blumenthal <markblumenthal@live.com>
2024-06-05 11:41:29 +0300

    changes

--- news-app-frontend/src/components/Navbar.tsx
+++ news-app-frontend/src/components/Navbar.tsx
@@ -1,10 +1,68 @@
 // src/components/Navbar.tsx
+// import React from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { RootState } from '../app/store';
+// import { logout } from '../features/auth/authSlice';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const Navbar: React.FC = () => {
+//   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const handleLogout = () => {
+//     dispatch(logout());
+//     navigate('/'); // Redirect to home page
+//   };
+
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <div className="container-fluid">
+//         <Link className="navbar-brand" to="/">NewsApp</Link>
+//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarNav">
+//           <ul className="navbar-nav">
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/politics">Politics</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/about">About</Link>
+//             </li>
+//             {isAuthenticated ? (
+//               <>
+//                 <li className="nav-item">
+//                   <Link className="nav-link" to="/preferences">Preferences</Link>
+//                 </li>
+//                 <li className="nav-item">
+//                   <button className="nav-link btn btn-link" onClick={handleLogout}>Logout</button>
+//                 </li>
+//               </>
+//             ) : (
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/user/register">Login/Register</Link>
+//               </li>
+//             )}
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+
+
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../app/store';
 import { logout } from '../features/auth/authSlice';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css'; // Import the custom CSS file
 
 const Navbar: React.FC = () => {
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
@@ -17,32 +75,32 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-black">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">NewsApp</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/politics">Politics</Link>
+              <Link className="nav-link text-white" to="/politics">Politics</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link text-white" to="/about">About</Link>
             </li>
             {isAuthenticated ? (
               <>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/preferences">Preferences</Link>
+                  <Link className="nav-link text-white" to="/preferences">Preferences</Link>
                 </li>
                 <li className="nav-item">
-                  <button className="nav-link btn btn-link" onClick={handleLogout}>Logout</button>
+                  <button className="nav-link btn btn-link text-white" onClick={handleLogout}>Logout</button>
                 </li>
               </>
             ) : (
               <li className="nav-item">
-                <Link className="nav-link" to="/user/register">Login/Register</Link>
+                <Link className="nav-link text-white" to="/user/register">Login/Register</Link>
               </li>
             )}
           </ul>
@@ -53,3 +111,4 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
+
