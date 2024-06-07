f9022e35128ed294c2c29fe747b2203ef9af7220
Mark Blumenthal <markblumenthal@live.com>
2024-06-07 11:15:29 +0300

    changes

--- news-app-frontend/src/App.tsx
+++ news-app-frontend/src/App.tsx
@@ -1,4 +1,88 @@
 // src/App.tsx
+// import React, { useEffect } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { RootState, AppDispatch } from './app/store';
+// import { fetchHomeArticles } from './features/articles/articlesSlice';
+// import ArticleCard from './components/ArticleCard';
+// import Navbar from './components/Navbar';
+// import Politics from './pages/Politics';
+// import About from './pages/About';
+// import RegisterLogin from './pages/Register';
+// import Preference from './pages/Preference';
+// import ProtectedRoute from './components/ProtectedRoute';
+// import PieChart from './components/PieChart';
+// import Footer from './components/Footer';
+// import VideoBackground from './components/VideoBackground';
+// import './App.css';
+
+// const Home: React.FC = () => {
+//   const dispatch: AppDispatch = useDispatch();
+//   const homeArticles = useSelector((state: RootState) => state.articles.homeArticles);
+//   const homeStatus = useSelector((state: RootState) => state.articles.homeStatus);
+
+//   useEffect(() => {
+//     if (homeStatus === 'idle') {
+//       dispatch(fetchHomeArticles());
+//     }
+//   }, [homeStatus, dispatch]);
+
+//   const cnnArticles = homeArticles.filter(article => article.source === 'cnn');
+//   const foxArticles = homeArticles.filter(article => article.source === 'fox-news');
+
+//   const pairedArticles = [];
+//   for (let i = 0; i < Math.max(cnnArticles.length, foxArticles.length); i++) {
+//     pairedArticles.push([cnnArticles[i], foxArticles[i]]);
+//   }
+
+//   return (
+//     <>
+//       <VideoBackground />
+//       <div className="moved-up-container container">
+//         <h1 className="my-4">Political News from the Liberal Left & Conservative Right</h1>
+//         <h3 className="my-4">Letting you choose what News you want to read</h3>
+//         <div className="row">
+//           {pairedArticles.map((pair, index) => (
+//             <div className="row mb-4" key={index}>
+//               <div className="col-md-6 col-sm-12 mb-4">
+//                 {pair[0] && <ArticleCard article={pair[0]} />}
+//               </div>
+//               <div className="col-md-6 col-sm-12 mb-4">
+//                 {pair[1] && <ArticleCard article={pair[1]} />}
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//         <PieChart />
+//         <Footer />
+//       </div>
+//     </>
+//   );
+// };
+
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/politics" element={<Politics />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/user/register" element={<RegisterLogin />} />
+//         <Route path="/preferences" element={<ProtectedRoute><Preference /></ProtectedRoute>} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+// src/App.tsx
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -54,8 +138,8 @@ const Home: React.FC = () => {
           ))}
         </div>
         <PieChart />
-        <Footer />
       </div>
+      <Footer />
     </>
   );
 };
@@ -76,6 +160,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-
-
--- news-app-frontend/src/pages/Preference.tsx
+++ news-app-frontend/src/pages/Preference.tsx
@@ -1,3 +1,66 @@
+// // src/pages/Preference.tsx
+// import React, { useState, useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { RootState, AppDispatch } from '../app/store';
+// import { fetchArticles } from '../features/articles/articlesSlice';
+// import ArticleCard from '../components/ArticleCard';
+// import PieChart from '../components/PieChart';
+// import { selectAllArticles } from '../features/articles/articlesSelectors';
+// import Footer from '../components/Footer';
+
+// const Preference: React.FC = () => {
+//   const [preference, setPreference] = useState<'liberal' | 'conservative'>('liberal');
+//   const dispatch: AppDispatch = useDispatch();
+//   const articles = useSelector(selectAllArticles);
+//   const articleStatus = useSelector((state: RootState) => state.articles.status);
+
+//   useEffect(() => {
+//     if (articleStatus === 'idle') {
+//       dispatch(fetchArticles());
+//     }
+//   }, [articleStatus, dispatch]);
+
+//   const filteredArticles = articles.filter(article =>
+//     preference === 'liberal' ? article.source === 'cnn' : article.source === 'fox-news'
+//   ).slice(0, 12);
+
+//   return (
+//     <div className="container">
+//       <h1 className="my-4">Select Your News Preference</h1>
+//       <div className="btn-group mb-4" role="group" aria-label="News Preference">
+//         <button
+//           type="button"
+//           className={`btn btn-primary ${preference === 'liberal' ? 'active' : ''}`}
+//           onClick={() => setPreference('liberal')}
+//         >
+//           Liberal Media
+//         </button>
+//         <button
+//           type="button"
+//           className={`btn btn-primary ${preference === 'conservative' ? 'active' : ''}`}
+//           onClick={() => setPreference('conservative')}
+//         >
+//           Conservative Media
+//         </button>
+//       </div>
+//       <div className="row">
+//         {filteredArticles.map(article => (
+//           <div className="col-md-6 col-lg-4 mb-4" key={article.id}>
+//             <ArticleCard article={article} />
+//           </div>
+//         ))}
+//       </div>
+//       <PieChart />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Preference;
+
+
+
+
 // src/pages/Preference.tsx
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -25,38 +88,41 @@ const Preference: React.FC = () => {
   ).slice(0, 12);
 
   return (
-    <div className="container">
-      <h1 className="my-4">Select Your News Preference</h1>
-      <div className="btn-group mb-4" role="group" aria-label="News Preference">
-        <button
-          type="button"
-          className={`btn btn-primary ${preference === 'liberal' ? 'active' : ''}`}
-          onClick={() => setPreference('liberal')}
-        >
-          Liberal Media
-        </button>
-        <button
-          type="button"
-          className={`btn btn-primary ${preference === 'conservative' ? 'active' : ''}`}
-          onClick={() => setPreference('conservative')}
-        >
-          Conservative Media
-        </button>
+    <>
+      <div className="container">
+        <h1 className="my-4">Select Your News Preference</h1>
+        <div className="btn-group mb-4" role="group" aria-label="News Preference">
+          <button
+            type="button"
+            className={`btn btn-primary ${preference === 'liberal' ? 'active' : ''}`}
+            onClick={() => setPreference('liberal')}
+          >
+            Liberal Media
+          </button>
+          <button
+            type="button"
+            className={`btn btn-primary ${preference === 'conservative' ? 'active' : ''}`}
+            onClick={() => setPreference('conservative')}
+          >
+            Conservative Media
+          </button>
+        </div>
+        <div className="row">
+          {filteredArticles.map(article => (
+            <div className="col-md-6 col-lg-4 mb-4" key={article.id}>
+              <ArticleCard article={article} />
+            </div>
+          ))}
+        </div>
+        <PieChart />
       </div>
-      <div className="row">
-        {filteredArticles.map(article => (
-          <div className="col-md-6 col-lg-4 mb-4" key={article.id}>
-            <ArticleCard article={article} />
-          </div>
-        ))}
-      </div>
-      <PieChart />
       <Footer />
-    </div>
+    </>
   );
 };
 
 export default Preference;
+
 
 
 
